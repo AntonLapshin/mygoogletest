7346ce73d35f9aa011f791019748e0c1833acaf1
Anton Lapshin <antvallap@gmail.com>
2014-06-12 11:45:19 +0700

    Removed build folder

--- build/index.js
+++
@@ -1,1 +0,0 @@
-var http=require("http"),fs=require("fs"),app=require("./app"),port=Number(process.env.PORT||8080);http.createServer(app.handler).listen(port),console.log("Google test http: "+port);
