79d1039a98a29ff8e8d8bc4950c3df8d9f03e742
Anton Lapshin <antvallap@gmail.com>
2014-07-04 18:30:21 +0700

    Changes in ad

--- project/www/app/js/ad.js
+++ project/www/app/js/ad.js
@@ -2,25 +2,31 @@ define(['model/ad'], function (ad) {
 
     var ID_GROUP = '72833562';
 
-    function srcToHttps(src){
+    function random(min, max) {
+        return (Math.random() * (max - min) + min) | 0
+    }
+
+    function srcToHttps(src) {
         return "https://pp.vk.me/c" + src.replace("http://cs", "").replace(".vk.me", "");
     }
 
-    function parseUser(data){
+    function parseUser(data) {
         return { id: data.uid || data.id, img: srcToHttps(data.photo_50), name: data.first_name + ' ' + data.last_name };
     }
 
     return {
-        getAdItem: function(user){
-            ad.forEach(function(item){
+        getAdItem: function (user) {
+            var max = 0;
+
+            ad.forEach(function (item) {
                 item.score = 0;
                 if (user.sex && user.sex === item.sex) item.score++;
                 if (user.city && user.city === item.city) item.score++;
 
-                if (user.bdate){
+                if (user.bdate) {
                     var dateValues = user.bdate.split('.');
                     var lastValue = dateValues[dateValues.length - 1];
-                    if (lastValue.length === 4){
+                    if (lastValue.length === 4) {
                         var year = parseInt(lastValue);
                         var age = (new Date()).getFullYear() - year;
                         if (age >= item.ageMin)
@@ -29,13 +35,20 @@ define(['model/ad'], function (ad) {
                             item.score++;
                     }
                 }
+                if (item.score > max)
+                    max = item.score;
             });
 
-            ad.sort(function(a, b){
-                return b.score - a.score;
+            var maxItems = [];
+            ad.forEach(function (item) {
+                if (item.score === max)
+                    maxItems.push(item);
             });
+//            ad.sort(function(a, b){
+//                return b.score - a.score;
+//            });
 
-            return ad[0];
+            return maxItems[random(0, maxItems.length)];
         }
     }
 });
--- project/www/app/model/ad.js
+++ project/www/app/model/ad.js
@@ -1,9 +1,125 @@
 define(function () {
 
     return [
-        { name: 'Кеды женские', sex: 2, city: 99, ageMin: 15, ageMax: 30, price: '890 р.',
-            img: '//cs614827.vk.me/v614827907/11577/bL6DuCk7jCo.jpg', link: '//vk.com/saymeplease?z=photo208699907_333167557%2Fwall-52587054_2110' },
-        { name: 'Часы', sex: 1, city: 99, price: '290 р.',
-            img: '//pp.vk.me/c618818/v618818907/42cb/mLBFxStIizM.jpg', link: '//vk.com/saymeplease?z=photo208699907_329200922%2Fwall-52587054_2087' }
+        {
+            name: 'Кеды женские',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 30,
+            price: '890 р.',
+            img: '//pp.vk.me/c614827/v614827907/11592/aFD7fpjBWuQ.jpg',
+            link: '//vk.com/saymeplease?z=photo208699907_333167557%2Fwall-52587054_2110'
+        },
+        {
+            name: 'Спортивный топ',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '250 р.',
+            img: '//pp.vk.me/c322831/v322831907/1b4c/TgjCQ17khBY.jpg',
+            link: '//vk.com/photo208699907_306965322'
+        },
+        {
+            name: 'Спортивный топ',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '250 р.',
+            img: '//cs322831.vk.me/v322831907/1858/_l6HzNtlCSM.jpg',
+            link: '//vk.com/photo208699907_306506963'
+        },
+        {
+            name: 'Горный велосипед',
+            sex: 2,
+            city: 99,
+            ageMin: 13,
+            ageMax: 50,
+            price: '13500 р.',
+            img: '//30.img.avito.st/640x480/885867830.jpg',
+            link: '//www.avito.ru/novosibirsk/velosipedy/velosiped_phillips_356799007'
+        },
+        {
+            name: 'Телефон Land Rover',
+            sex: 2,
+            city: 99,
+            ageMin: 10,
+            ageMax: 60,
+            price: '2700 р.',
+            img: '//35.img.avito.st/640x480/886192635.jpg',
+            link: '//www.avito.ru/novosibirsk/telefony/telefon_land_rover_x6_356916089'
+        },
+        {
+            name: 'Телефон Lenovo',
+            sex: 1,
+            city: 99,
+            ageMin: 10,
+            ageMax: 50,
+            price: '4900 р.',
+            img: '//40.img.avito.st/640x480/883902240.jpg',
+            link: '//www.avito.ru/novosibirsk/telefony/novyy_telefon_lenovo_356160986'
+        },
+        {
+            name: 'Кроссовки AirMax',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '1200 р.',
+            img: '//cs620223.vk.me/v620223907/e1e8/IT4nYF46zNY.jpg',
+            link: '//vk.com/photo208699907_334057488'
+        },
+        {
+            name: 'Кроссовки AirMax',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '1200 р.',
+            img: '//cs620223.vk.me/v620223907/e1d6/YmXf6kE29M0.jpg',
+            link: '//vk.com/photo208699907_334057485'
+        },
+        {
+            name: 'Свитшот женский',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '800 р.',
+            img: '//cs620223.vk.me/v620223907/dda9/mNdi1TY8CDg.jpg',
+            link: '//vk.com/photo208699907_333989747'
+        },
+        {
+            name: 'Свитшот женский',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '800 р.',
+            img: '//cs620223.vk.me/v620223907/dda0/LuqASa2u4fg.jpg',
+            link: '//vk.com/photo208699907_333989744'
+        },
+        {
+            name: 'Свитшот женский',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '800 р.',
+            img: '//cs620223.vk.me/v620223907/ddbb/Au7SwvkohnQ.jpg',
+            link: '//vk.com/photo208699907_333989753'
+        },
+        {
+            name: 'Свитшот женский',
+            sex: 1,
+            city: 99,
+            ageMin: 15,
+            ageMax: 35,
+            price: '800 р.',
+            img: '//cs620223.vk.me/v620223907/ddb2/h8co5xFwX7o.jpg',
+            link: '//vk.com/photo208699907_333989751'
+        },
     ]
 });
